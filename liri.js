68dd3896b8a69b075ab3ea09ecd01f8ae47717a5
john <JohnBassili@yahoo.com>
2018-05-04 23:56:37 -0500

    liri push

---
+++ liri.js
@@ -0,0 +1,14 @@
+
+
+// Make it so liri.js can take in one of the following commands:
+
+// * `my-tweets`
+// * `spotify-this-song`
+// * `movie-this`
+// * `do-what-it-says`
+
+
+
+
+
+require("dotenv").config();
